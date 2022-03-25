0061bb949cb0cf0d9c3a9e165ee7bdcd714f126f
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-03-25 18:45:24 -0400

    set localStorage

--- README.md
+++ README.md
@@ -9,11 +9,11 @@ GIVEN I am using a daily planner to create a schedule
 *WHEN I scroll down*
 *THEN I am presented with time blocks for standard business hours*
 
-WHEN I view the time blocks for that day
-THEN each time block is color-coded to indicate whether it is in the past, present, or future
+*WHEN I view the time blocks for that day*
+*THEN each time block is color-coded to indicate whether it is in the past, present, or future*
 
-*WHEN I click into a time block*
-*THEN I can enter an event*
+WHEN I click into a time block
+THEN I can enter an event
 
 WHEN I click the save button for that time block
 THEN the text for that event is saved in local storage
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,54 @@
+// display current day
 $("#currentDay").text(moment().format("LLLL"));
 
 var time = ['0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700']
 
-
-
-function loadTask() {
+function appendDiv() {
     // create HTML DOM elements
     time.forEach(hour => {
 
         $('.container').append(`
         <div class="row time-block">
             <div class="col-md-1 hour">${hour}hrs</div>
-            <textarea class="col-md-10 hour-block" id="${hour}"></textarea>
-            <button class="col-md-1 saveBtn" id="saveBtn"><span class="oi oi-lock-locked"></span></button>
+            <textarea class="col-md-10 hour-block description" id="${hour}"></textarea>
+            <button class="col-md-1 saveBtn"><span class="oi oi-lock-locked"></span></button>
         </div>
     `)
     });
 };
 
 function createTask() {
-    loadTask();
+    appendDiv();
+    
     // get current time and set it to military time
     var currentTime = moment().format("HH" + "00");
-    console.log(currentTime);
 
     // loop through each hour to set corresponding color blocks
-    $(".time-block").each(function(){
+    $(".time-block").each(function () {
+
         var hrBlock = ($(this).find(".hour").text().split("hrs")[0]);
-        console.log(hrBlock);
+        $(this).find(".saveBtn").attr("id", hrBlock);
+
         if (currentTime < hrBlock) {
-            console.log("this time is future " + hrBlock)
-            $(this).find(".hour-block").addClass("future");
+            $(this).find(".hour-block").addClass("future").attr("id", hrBlock);
         } else if (currentTime > hrBlock) {
-            console.log("this time is past " + hrBlock)
-            $(this).find(".hour-block").addClass("past");
+            $(this).find(".hour-block").addClass("past").attr("id", hrBlock);
         } else {
-            console.log("this time is present " + hrBlock)
-            $(this).find(".hour-block").addClass("present");
+            $(this).find(".hour-block").addClass("present").attr("id", hrBlock);
         }
+
     });
 }
 
+// localStorage the hr id value and text description
+$(".container").on("click", "button", function () {
+
+    var text = $(this).siblings(".description").val();
+    var time = $(this).attr("id");
+    
+    localStorage.setItem(time, text);
+});
+
+
 createTask();
+
